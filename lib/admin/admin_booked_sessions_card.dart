f918c69d00a83d2bb9cc353b811d7d8d49aeb8aa
Bhaksar <bhaskarmandal369@gmail.com>
2023-10-24 04:47:36 +0530

    delete booked sessions added

--- lib/admin/admin_booked_sessions_card.dart
+++ lib/admin/admin_booked_sessions_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sunshine_iith/admin/bottomsheet_booked_sessions.dart';
+import 'package:sunshine_iith/services/rtdb_database.dart';
 import 'package:sunshine_iith/services/session_data.dart';
 
 class AdminBookSessionCard extends StatelessWidget {
@@ -7,47 +8,77 @@ class AdminBookSessionCard extends StatelessWidget {
   final String mode;
   final SessionData sessionData;
 
-  const AdminBookSessionCard({super.key,required this.time,required this.mode,required this.sessionData});
+  const AdminBookSessionCard(
+      {super.key,
+      required this.time,
+      required this.mode,
+      required this.sessionData});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width*0.8,
+      width: MediaQuery.of(context).size.width * 0.8,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.orange,width: 1.8)
-      ),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.orange, width: 1.8)),
       child: Padding(
         padding: const EdgeInsets.all(0.0),
         child: ListTile(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Text(
-            time,
-            style: const TextStyle(
-              color: Colors.orange,
-              fontSize: 20,
-              fontWeight: FontWeight.w400
+          title: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              time,
+              style: const TextStyle(
+                  color: Colors.orange,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400),
             ),
           ),
-        ),
-        subtitle: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Text('Message: $mode',style: TextStyle(
-              color: Colors.orange.withOpacity(0.8),
-              fontSize: 16,
-              fontWeight: FontWeight.w300
-            ),),
-        ),
-        trailing: InkWell(
-          onTap:(){
-            //Show pop up and delete
-          },
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          child: const Icon(Icons.delete_rounded,size: 24, color: Colors.orange)),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              'Message: $mode',
+              style: TextStyle(
+                  color: Colors.orange.withOpacity(0.8),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300),
+            ),
           ),
+          trailing: InkWell(
+              onTap: () {
+                //Show pop up and delete
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return showPopUp(context, sessionData);
+                    });
+                print('Delete button clicked');
+              },
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: const Icon(Icons.delete_rounded,
+                  size: 24, color: Colors.orange)),
+        ),
       ),
+    );
+  }
+
+  Widget showPopUp(BuildContext context, SessionData data) {
+    return AlertDialog(
+      content: const Text('Are you sure you want to delete this session?'),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context, 'Cancle'),
+          child: const Text('Cancle'),
+        ),
+        TextButton(
+          onPressed: ()  {
+            RealTimeDB().deleteSessionData(data);
+            Navigator.pop(context,'Delete');
+          },
+          child: const Text('Delete'),
+        ),
+      ],
     );
   }
 }
--- lib/services/rtdb_database.dart
+++ lib/services/rtdb_database.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:sunshine_iith/services/session_data.dart';
 
-
 class RealTimeDB {
   Future<Map<String, dynamic>> getAppointmentList(String uid) async {
     final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
     try {
-      DatabaseEvent event =
-          await databaseReference.child('sessions').child(uid).orderByChild('time').once();
+      DatabaseEvent event = await databaseReference
+          .child('sessions')
+          .child(uid)
+          .orderByChild('time')
+          .once();
 
       DataSnapshot dataSnapshot = event.snapshot;
 
@@ -55,7 +57,9 @@ class RealTimeDB {
     }
   }
 
-  void addSessionData(SessionData data,) {
+  void addSessionData(
+    SessionData data,
+  ) {
     final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
 
     // DatabaseReference sessionReference =
@@ -70,7 +74,7 @@ class RealTimeDB {
         .child(counsellor)
         .child('sessions')
         .child(date)
-        .push();
+        .child(data.time);
 
     Map<String, dynamic> sessionDataMap = data.toMap();
 
@@ -84,6 +88,30 @@ class RealTimeDB {
       print('Session data uploaded to counsellors reference successfully');
     }).catchError((error) {
       print('Error uploading session data: $error');
+    });
+  }
+
+  void deleteSessionData(SessionData data) {
+    final DatabaseReference databaseReference =
+        FirebaseDatabase.instance.ref();
+
+    String date = convertDateFormat(data.date);
+    String counsellor = data.counsellorsName;
+    if (data.counsellorsName.contains('.')) {
+      counsellor = 'Phani Bhushan';
+    }
+
+    DatabaseReference counsellorReference = databaseReference
+        .child(counsellor)
+        .child('sessions')
+        .child(date)
+        .child(data.time);
+
+    // Remove the session data at the specified reference
+    counsellorReference.remove().then((_) {
+      print('Session data deleted successfully');
+    }).catchError((error) {
+      print('Error deleting session data: $error');
     });
   }
 
